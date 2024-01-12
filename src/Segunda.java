553e15241ebb70b55c1fe716bf76119d52b0370c
pabloieshna <joesingleton417@gmail.com>
2024-01-12 12:47:02 +0100

    ejercicio 40

--- src/Segunda.java
+++ src/Segunda.java
@@ -1,16 +1,18 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class Primera {
+public class Segunda {
+
 	public static void main(String[] args) {
 		Scanner leer = new Scanner(System.in);
 		System.out.println("introduce un valor mayor que 5");
 		int nextInt = leer.nextInt();
-		if (nextInt > 5) {
-			System.out.println("muy bien es mayor");
-		} else
-
-		{
-			System.out.println("pero que te he dicho?");
+		boolean sigo=true;
+		for (int i = 0; i < nextInt&&sigo; i++) {
+			System.out.println("hola");
+			sigo=new Random().nextBoolean();
 		}
+
 	}
+
 }
